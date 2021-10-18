f6b1d34c385e28be5cc691a94bf6f36b06dc32db
shushrut <shushrutdeshpande@shushruts-MacBook-Pro.local>
2021-10-19 01:44:58 +0530

    tab navigation added

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function SampleTabs() {
+
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+      <Tab.Screen name="SettingsScreen" component={SettingsScreen} />
+    </Tab.Navigator>
+
+  );
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Basaya bondaragi welcome</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <SampleTabs />
+    </NavigationContainer>
+
   );
 }
 
