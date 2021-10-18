5d53addaa84da3a840501fc5e44382990ff9f5cf
shushrut <shushrutdeshpande@shushruts-MacBook-Pro.local>
2021-10-19 01:14:07 +0530

    second commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>shushruth welcome</Text>
+      <Text>Pande welcome</Text>
       <StatusBar style="auto" />
     </View>
   );
