5c599f4174e5ad9583596bdad8b3aca883be54ee
shushrut <shushrutdeshpande@shushruts-MacBook-Pro.local>
2021-10-19 01:18:05 +0530

    final checkin

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Pande welcome</Text>
+      <Text>Basaya  welcome</Text>
       <StatusBar style="auto" />
     </View>
   );
